92980904750ad238a48e7db2802c239c34b68a1e
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2025-06-28 12:17:01 +0100

    [Parsed content-length + FieldeName & FieldeValue in headers]

--- Request/Request.hpp
+++ Request/Request.hpp
@@ -1,23 +1,29 @@
 #pragma once
 
+#include "Get.hpp"
+#include "Post.hpp"
+#include "Delete.hpp"
 #include "../allincludes.hpp"
 #include "../pars_config/config.hpp"
 
 #define BUFFER_SIZE 1024 // 1kb
 #define MAX_HEADER_SIZE 8192 // 8kb
 
-/**
- * 
- * 
- */
+enum Method
+{
+	GET,
+	POST,
+	DELETE
+};
 
-class Request
+class	Request
 {
 private:
 	int						ClientFd;
 	std::vector<ConfigNode>	Servers;
 	ConfigNode				RightServer;
 
+	Method								Method;
 	std::map<std::string, std::string>	Headers;
 	std::map<std::string, std::string>	QueryParams;
 	std::string							FullSystemPath;
@@ -27,6 +33,20 @@ private:
 	std::string					BodyUnprocessedBuffer;
 	size_t						ContentLength;
 	
+public:
+	Request(const int	&, std::vector<ConfigNode>);
+	~Request();
+	
+	// ---------		GETTERS 	 	--------- //
+	std::map<std::string, std::string>	GetHeaders() const;
+	std::string		GetFullPath() const;
+	std::string		GetHeaderValue(std::string) const;
+	bool			GetConnection() const;
+	
+	// ---------		SETTERS 	 	--------- //
+	void	SetHeaderValue(std::string, std::string);
+	
+	// ---------	MEMBER FUNCTIONS 	--------- //
 	void	ReadRequestHeader();
 	void	ReadFirstLine(std::string);
 	void	ReadHeaders(std::string);
@@ -37,19 +57,6 @@ private:
 	void	HandlePath();
 	void	SplitURI();
 	void	ParseURI(std::string	&URI);
-public:
-	Request(const int	&, std::vector<ConfigNode>);
-	~Request();
-
-	// ---------		GETTERS 	 	--------- //
-	std::map<std::string, std::string>	GetHeaders() const;
-	std::string		GetHeaderValue(std::string) const;
-	bool			GetConnection() const;
-
-	// ---------		SETTERS 	 	--------- //
-	void	SetHeaderValue(std::string, std::string);
-
-	// ---------	MEMBER FUNCTIONS 	--------- //
 	void	SetUpRequest();
 };
 
@@ -59,3 +66,5 @@ bool			IsHexa(char c);
 std::string		HexaToChar(std::string	Hexa);
 void			PrintHeaders(std::map<std::string, std::string> Headers);
 bool			ValidContentLength(const std::string& value);
+bool			ValidFieldName(const std::string& name);
+bool			ValidFieldValue(const std::string& value);
