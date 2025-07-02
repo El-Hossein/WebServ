22b7f19e4f0c094004e3a36b611a7891f0600de4
Ismail Rkik <isrkik@e3r8p12.1337.ma>
2025-07-02 16:53:25 +0100

    fix something hh

--- Response/responseMain.cpp
+++ Response/responseMain.cpp
@@ -7,37 +7,33 @@ void    moveToResponse(int &client_fd, std::map<int, std::string>& response_map,
 
     if (IsCgiRequest(uri.c_str()))
     {
-        // std::cout << "la hna" << std::endl;
-        // exit (2);
         finalResponse = handleCgiRequest(req);
     }
     else
     {
-        std::string _PATH = "." + uri;
+        std::string _PATH = uri; // i need to combine the root directory with the uri
 
         std::ifstream inFile(_PATH.c_str(), std::ios::binary);
         if (inFile)
         {
             std::ostringstream outStringFIle;
             outStringFIle << inFile.rdbuf(); // so i can read the whole file
-
+            inFile.close();
             std::string fileBody = outStringFIle.str();
             finalResponse = "HTTP/1.1 200 OK\r\n";
             finalResponse += "Content-Length: ";
-            finalResponse += intToString(fileBody.size());
-            finalResponse += "\r\n";
+            finalResponse += intToString(fileBody.size()) + "\r\n";
+            finalResponse += "Connection: close\r\n";
             finalResponse += "Content-Type: text/html\r\n";
             finalResponse += "\r\n";
             finalResponse += fileBody;
         }
         else
         {
-            finalResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+            // and need to display given error page
+            finalResponse += responseError(404, " Not Found"); // just a test i have to catsh error codes ziad throw
         }
-        // std::cout << "hna" << std::endl;
-        // exit (1);
-
     }
-    // std::cout << "hna " << std::endl;
+    std::cout << finalResponse << std::endl;
     response_map[client_fd] = finalResponse;
 }
