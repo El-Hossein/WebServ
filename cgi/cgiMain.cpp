88917e085d0f8d3aa1c167eb8ab308a846652f27
ismail <i61mail@ismail.local>
2025-07-08 03:40:16 -0700

    handle cgi PATH_INFO, split uri from it, handle env variables for cgi, work with request attributes

--- cgi/cgiMain.cpp
+++ cgi/cgiMain.cpp
@@ -1,11 +1,10 @@
 #include "cgiHeader.hpp"
 
-
 std::string intToString(int n)
 {
-    char buffer[32];
-    sprintf(buffer, "%d", n);
-    return std::string(buffer);
+    std::ostringstream oss;
+    oss << n;
+    return oss.str();
 }
 
 
@@ -25,18 +24,45 @@ int fileChecking(const std::string& path)
     return 0;
 }
 
+pathInfo splitPathInfo(const Request &req)
+{
+    pathInfo _pathinfo;
+    _pathinfo._pathInfo = "";
+    std::string fullPath = req.GetFullPath();
+
+    size_t quotationPos = fullPath.find('?');
+    if (quotationPos != std::string::npos)
+        fullPath = fullPath.substr(0, quotationPos);
+
+    size_t posLength = std::string::npos;
+    size_t cgiPos = fullPath.find(".cgi");
+    size_t phpPos = fullPath.find(".php");
+    size_t pyPos = fullPath.find(".py");
+    if (cgiPos != std::string::npos && cgiPos < phpPos && cgiPos < pyPos)
+        posLength = cgiPos + 4;
+    else if (phpPos != std::string::npos && phpPos < cgiPos && phpPos < pyPos)
+        posLength = phpPos + 4;
+    else if (pyPos != std::string::npos && pyPos < cgiPos && pyPos < phpPos)
+        posLength = pyPos + 3;
+
+    _pathinfo.scriptFile = fullPath.substr(0, posLength);
+    if (posLength < fullPath.length())
+        _pathinfo._pathInfo = fullPath.substr(posLength);
+    return _pathinfo;
+}
+
 std::string handleCgiRequest(const Request &req, std::vector<ConfigNode> ConfigPars)
 {
     const char *requestBody = "";
 
-    // need to check which methode if its not get or post error.
-    std::string scriptPath = req.GetFullPath();
+    pathInfo _pathinfo = splitPathInfo(req);
+    std::string scriptPath = _pathinfo.scriptFile;
     int code = fileChecking(scriptPath);
     if (code == 403)
         return responseError(403, " Forbidden", ConfigPars);
     else if (code == 404)
         return responseError(404, " not found", ConfigPars);
-    std::string scriptOutput = executeCgiScript(scriptPath.c_str(), req, ConfigPars);
+    std::string scriptOutput = executeCgiScript(scriptPath.c_str(), req, ConfigPars, _pathinfo._pathInfo);
     if (scriptOutput.empty())
         return responseError(500, " internal server error", ConfigPars);
     CgiResponse parsedCgi = parseOutput(scriptOutput);
@@ -45,10 +71,9 @@ std::string handleCgiRequest(const Request &req, std::vector<ConfigNode> ConfigP
 
 int IsCgiRequest(const char *uri)
 {
-    //make a specific folder for CGI scripts
     std::string uriString(uri);
     size_t index = uriString.find("/cgiScripts/");
-    if (index == 0)
+    if (index == std::string::npos)
         return 0;
     std::string pathAfterCgi = uriString.substr(index + 12);
     if (pathAfterCgi.empty())
@@ -56,22 +81,10 @@ int IsCgiRequest(const char *uri)
     size_t firstSlash = pathAfterCgi.find("/");
     std::string scriptFile;
     if (firstSlash != std::string::npos)
-    {
         scriptFile = pathAfterCgi.substr(0, firstSlash);
-    }
-    // need to complete here
-    
-    std::cout << "hnaaa" << std::endl;
-
-    exit(1);
+    else
+        scriptFile = pathAfterCgi;
+    if (scriptFile.find(".cgi") != std::string::npos || scriptFile.find(".py") != std::string::npos || scriptFile.find(".php") != std::string::npos)
+        return 1;
     return 0;
 }
-
-    // const char *extension = std::strrchr(uri, '.');
-    // if (extension == NULL)
-    //     return 0;
-    // if (extension && (std::strcmp(extension, ".cgi") == 0 || std::strcmp(extension, ".py") == 0) || std::strcmp(extension, ".php") == 0)
-    // {
-        
-    //     return 1;
-    // }
