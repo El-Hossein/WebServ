179435e133d852469767e2eac3e2fef38084fb28
Hessain <eel-ghal@e3r1p7.1337.ma>
2025-07-14 17:25:29 +0100

    manage huge req

--- AllServer/HttpServer.hpp
+++ AllServer/HttpServer.hpp
@@ -1,3 +1,5 @@
+#ifndef HTTPSERVER_HPP
+#define HTTPSERVER_HPP
 #pragma once
 
 #define BACKLOG 128
@@ -10,18 +12,19 @@
 #include "../Response/responseHeader.hpp"
 
 class HttpServer{
-    public:
-        HttpServer();
-        HttpServer(const HttpServer& other);
-        ~HttpServer();
+	public:
+		HttpServer();
+		HttpServer(const HttpServer& other);
+		~HttpServer();
 
-        void setup_server(std::vector<ConfigNode> ConfigPars);
-        void run(std::vector<ConfigNode> ConfigPars);
-        void accept_new_client(int server_fd);
-        void handle_client(int client_fd, int filter, std::vector<ConfigNode> ConfigPars);
-        void remove_client(int client_fd);
-    private:
-        int kq;
-        std::vector<int> server_fds;
-        std::map<int, std::string> response_map;
+		void setup_server(std::vector<ConfigNode> ConfigPars);
+		void run(std::vector<ConfigNode> ConfigPars);
+		Request * accept_new_client(int server_fd, std::vector<ConfigNode> ConfigPars);
+		void handle_client(int client_fd, struct kevent* event, std::vector<ConfigNode> ConfigPars, std::vector<Request * >& all);
+		void remove_client(int client_fd);
+	private:
+		int kq;
+		std::vector<int> server_fds;
+		std::map<int, std::string> response_map;
 };
+#endif
