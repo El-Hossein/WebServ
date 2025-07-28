199956bcc0b18a04f92ab0bdf4c4407391ae3279
El hossein El ghalbzouri <eel-ghal@e3r7p13.1337.ma>
2025-07-28 21:01:39 +0100

    added getlocation

--- pars_config/config.hpp
+++ pars_config/config.hpp
@@ -23,14 +23,16 @@ class ConfigNode {
 		void addValue(const std::string& key, const std::string& value);
 		void addChild(const ConfigNode& child);
 		ConfigNode& getLastChild();
-		// const std::vector<ConfigNode>& getChildren() const;
+		const std::vector<ConfigNode>& getChildren() const;
 		const std::string& getName() const;
 		void PutName(const std::string& name);
 		
 		std::map<std::string, std::vector<std::string> >& getValues() ;
 		static ConfigNode GetServer(std::vector<ConfigNode> ConfigPars, _ServerDetails ServerDetails);
 		static std::vector<std::string> getValuesForKey(ConfigNode& ConfNode, const std::string& key, std::string del)  ;
+		static std::vector<std::string> ConfgetValuesForKey(ConfigNode& ConfNode, const std::string& key, std::string del)  ;
 		static std::string GetLocationValue(ConfigNode& ConfNode, const std::string& key, size_t index)  ;
+		std::string GetRightLocation(std::string path);
 		// const std::map<std::string, std::vector<std::string> >& getValues() const {return values;}
 		void print() const;
 };
