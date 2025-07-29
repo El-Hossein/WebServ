94ae0ae1700d22320177d7979be06faae77a1871
Ziyad Derfoufi <zderfouf@e3r4p10.1337.ma>
2025-07-29 23:00:37 +0100

    [Boundary working Perfectly with Big and small files]

--- Request/Post.hpp
+++ Request/Post.hpp
@@ -20,17 +20,20 @@ private:
 
 	std::map<std::string, std::string>	BodyParams;
 	std::string							UnprocessedBuffer;
+
 	std::string							Filename;
+	std::ofstream						OutFile;
 
 	size_t			MaxAllowedBodySize;
 public:
 	Post(Request	&_obj);
 	~Post();
 
+	void	FindFileName(std::string	&Buffer, std::string	&Filename);
 	void	WriteToFile(std::string	&Filename, std::string &Buffer);
 	void	GetSubBodies(std::string &Buffer);
 	void	ParseChunked(std::string);
-	void	ParseBoundary(std::string);
+	void	ParseBoundary();
 	void	IsBodyFullyRead();
 	void	HandlePost();
 };
