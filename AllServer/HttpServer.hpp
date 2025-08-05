1ca797b0988115c87de9050704fb16c91ac43f4d
Ismail Rkik <isrkik@e3r8p12.1337.ma>
2025-08-05 11:23:58 +0100

    9smna lfunctions d server chwya w zdna logs f response

--- AllServer/HttpServer.hpp
+++ AllServer/HttpServer.hpp
@@ -21,9 +21,11 @@ class HttpServer{
 		void setup_server(std::vector<ConfigNode> ConfigPars);
 		void run(std::vector<ConfigNode> ConfigPars);
 		Request * accept_new_client(int server_fd, std::vector<ConfigNode> ConfigPars);
+		void	traiteCgiProcess(std::vector<Response*> all_res, int kq, std::vector<Request*> all_request, std::vector<ConfigNode> ConfigPars, int i);
 		void handle_client(int client_fd, struct kevent* event, std::vector<ConfigNode> ConfigPars, std::vector<Request * >& all, std::vector<Response *> & all_res);
 		void remove_client(int client_fd);
 	private:
+   		struct kevent events[BACKLOG];
 		int kq;
 		std::vector<int> server_fds;
 		std::map<int, std::string> response_map;
