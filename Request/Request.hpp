d70c2bb07192a9408ae1595f6986a17e7b6fb4f0
Ziyad Derfoufi <zderfouf@e3r6p7.1337.ma>
2025-08-12 12:09:29 +0100

    [Added the EventContext struct for The server part]

--- Request/Request.hpp
+++ Request/Request.hpp
@@ -27,7 +27,9 @@ enum	ContentType
 	_Boundary, BinaryOrRaw
 };
 
+class	Cgi;
 class	Post;
+struct	EventContext;
 
 class	Request
 {
@@ -66,6 +68,8 @@ private:
 public:
 	Request(const int	&, ClientStatus, std::vector<ConfigNode>, int &);
 	~Request();
+
+	EventContext* ctx;
 	// ---------		GETTERS 	 	--------- //
 	bool								GetConnection() const;
 	int									GetClientStatus() const;
@@ -88,6 +92,7 @@ public:
 	time_t    							GetTimeOut() const;
 
 	// ---------		SETTERS 	 	--------- //
+    void	SetContext(EventContext* ctx);
 	void	SetFullSystemPath(std::string	&Path);
 	void	SetExtentionsMap();
 	void	SetHeaderValue(std::string, std::string);
